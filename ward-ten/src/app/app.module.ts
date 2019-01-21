06dc5526a63de36f3d49e7bab187ec1f400dbdbf
Joshua Decosta <joshdecosta@gmail.com>
2019-01-21 09:29:02 -0800

    adds responsive capabilities for mobile

---
+++ ward-ten/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UiModule // new modules added here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
