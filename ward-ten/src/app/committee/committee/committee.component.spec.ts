c15738bacddb228e06faa036e9e3a39ed0fd3241
Joshua Decosta <josh@Joshuas-M1.lan>
2022-01-02 20:55:35 -0500

    updates cli form 10.2 to 11

--- ward-ten/src/app/committee/committee/committee.component.spec.ts
+++ ward-ten/src/app/committee/committee/committee.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CommitteeComponent } from './committee.component';
 
@@ -6,7 +6,7 @@ describe('CommitteeComponent', () => {
   let component: CommitteeComponent;
   let fixture: ComponentFixture<CommitteeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CommitteeComponent ]
     })
